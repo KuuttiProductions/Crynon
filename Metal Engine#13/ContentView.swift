d5a86e000ee7a80023000d4c25e30c52c632c15b
KudeKube <Kuutti.taavitsainen@icloud.com>
2023-05-28 18:38:58 +0300

    Initial Commit

---
+++ Metal Engine#13/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Metal Engine#13
+//
+//  Created by Kuutti Taavitsainen on 28.5.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Metal Engine#13/Metal_Engine_13App.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_Engine_13App.swift
+//  Metal Engine#13
+//
+//  Created by Kuutti Taavitsainen on 28.5.2023.
+//
+
+import SwiftUI
+
+@main
+struct Metal_Engine_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
